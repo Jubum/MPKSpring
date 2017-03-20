ace136fa267bc5eb2daa267ea97d39656cdab5ce
Jubum <snaodrn@naver.com>
2017-03-20 18:59:05 +0900

    Add files via upload

---
+++ 웹/92.js
@@ -0,0 +1,9 @@
+
+$(function(){
+	$(".btn1").on("click", function(){
+		$(".frame img").attr({"src":"a.jpg", "alt":"¾ó±¼1"});
+	});
+	$(".btn2").on("click", function(){
+		$(".frame img").attr({"src":"b.jpg", "alt":"¾ó±¼2"});
+	});
+})
---
+++ 웹/93.js
@@ -0,0 +1,14 @@
+
+$(function(){
+
+	/*$("li").children("span").css({"color":"red"}); */
+	$(".txt1").parent().css({"color":"red"});
+	$("span").silbings().css({"color":"red"});
+	$("ul").find(".abc").css({"color":"red"});
+	$("li").eq(2).css({"color":"red"});
+	$("li").eq(2).css({"color":"red"});
+	$("li").eq(2).css({"color":"red"});
+	$("li").eq(2).css({"color":"red"});
+	$("li").eq(2).css({"color":"red"});
+	
+})
